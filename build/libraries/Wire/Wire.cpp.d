7980467a014a3eb45e2cb498f74023511fed1260
DKNS-JCC <jorgecuadradojcc@usal.es>
2024-06-21 22:54:54 +0200

    Removed folder from repository

--- build/libraries/Wire/Wire.cpp.d
+++
@@ -1,8 +0,0 @@
-C:\Users\jorge\Desktop\own_deej\build\libraries\Wire\Wire.cpp.o: \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\Wire.cpp \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility/twi.h \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\Wire.h \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h
--- build/libraries/Wire/utility/twi.c.d
+++
@@ -1,7 +0,0 @@
-C:\Users\jorge\Desktop\own_deej\build\libraries\Wire\utility\twi.c.o: \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.c \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\eightanaloginputs/pins_arduino.h \
- c:\users\jorge\appdata\local\arduino15\packages\arduino\hardware\avr\1.8.6\variants\standard\pins_arduino.h \
- C:\Users\jorge\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.h
