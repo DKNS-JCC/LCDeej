ee666d3a830b1b600ac230c70e8e1d54f463a712
DKNS-JCC <jorgecuadradojcc@usal.es>
2024-06-21 22:53:01 +0200

    🥚 Some more EE

--- own_deej.ino
+++ own_deej.ino
@@ -6,33 +6,43 @@
 #define LCD_COLUMNS 16
 #define LCD_ROWS 2
 
-//JEJE
-byte cross [8] = {
-  B00000,
-  B10001,
-  B01010,
-  B00100,
-  B01010,
-  B10001,
-  B00000,
-  B00000
-};
+// JEJE
+byte cross[8] = {
+    B00000,
+    B10001,
+    B01010,
+    B00100,
+    B01010,
+    B10001,
+    B00000,
+    B00000};
 
-byte on [8] = {
-  B00000,
-  B00100,
-  B01110,
-  B10101,
-  B10001,
-  B01110,
-  B00000,
-  B00000
-};
+byte on[8] = {
+    B00000,
+    B00100,
+    B01110,
+    B10101,
+    B10001,
+    B01110,
+    B00000,
+    B00000};
+
+byte alien[8] = {
+    B00000,
+    B01110,
+    B11111,
+    B10101,
+    B01110,
+    B00100,
+    B00000,
+    B00000};
 
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
 String song = "";
-unsigned long previousMillis = 0;
+unsigned long interval = 7000; // Tiempo hasta que se apaga la retroiluminación
+unsigned long lastTime = 0;
+bool lcdOn = true;
 
 void setup()
 {
@@ -48,9 +58,20 @@ void loop()
   {
     if (Serial.read() == '*')
     {
-      song = Serial.readStringUntil('\n');
+      String song = Serial.readStringUntil('\n');
       showMessage(song);
+      lastTime = millis(); // Actualizar el tiempo de la última interacción
+      if (!lcdOn) {
+        lcdOn = true;
+        lcd.backlight(); // Encender la retroiluminación
+      }
     }
+  }
+
+  // Verificar si ha pasado el tiempo de inactividad
+  if (lcdOn && (millis() - lastTime >= interval)) {
+    lcd.noBacklight(); // Apagar la retroiluminación
+    lcdOn = false;
   }
 }
 
@@ -62,28 +83,34 @@ void showMessage(String message)
 
   if (index != -1)
   {                                        // Si se encuentra el carácter '*'
-    title = message.substring(0, index);  // Subcadena desde el inicio hasta el carácter '*'
+    title = message.substring(0, index);   // Subcadena desde el inicio hasta el carácter '*'
     artist = message.substring(index + 1); // Subcadena desde después del carácter '*' hasta el final
   }
-
 
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(title);
   lcd.setCursor(0, 1);
-  
+
   if (artist == "Martin Garrix")
   {
     lcd.print("Martin Garrix +");
     lcd.createChar(0, cross);
-    lcd.setCursor(15,1);
+    lcd.setCursor(15, 1);
     lcd.write(byte(0));
   }
-  else if(artist == "Justin Mylo")
+  else if (artist == "Justin Mylo")
   {
     lcd.print("Justin Mylo");
     lcd.createChar(0, on);
-    lcd.setCursor(12,1);
+    lcd.setCursor(12, 1);
+    lcd.write(byte(0));
+  }
+  else if (artist == "AREA21")
+  {
+    lcd.print("AREA21");
+    lcd.createChar(0, alien);
+    lcd.setCursor(7, 1);
     lcd.write(byte(0));
   }
   else
